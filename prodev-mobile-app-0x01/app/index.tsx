b930de1ddee47e69af8e77e06d36f55314604ead
Zakhele <prince.feela2905@gmail.com>
2025-08-29 22:38:20 +0200

    .

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,15 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.largeText}>
-        Typescript is great if you practice more
-      </Text>
-      <Text style={styles.mediumText}>
-        React Native provides you a single codebase for cross platforms
-      </Text>
-      <Text style={styles.smallText}>
-        ALX is awesome
-      </Text>
+      {/* Main Entry Text */}
+      <Text>Entry Screen - Awesome</Text>
+
+      {/* Additional Text Components */}
+      <View>
+        <Text style={styles.largeText}>
+          Typescript is great if you practice more
+        </Text>
+        <Text style={styles.mediumText}>
+          React Native provides you a single codebase for cross platforms
+        </Text>
+        <Text style={styles.smallText}>
+          ALX is awesome
+        </Text>
+      </View>
     </View>
   );
 }
@@ -22,20 +28,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#90caf9',
     padding: 20,
   },
   largeText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: 30,
+    color: '#f44336',
+    marginBottom: 5,
+    fontWeight: '700',
+    fontVariant: ['small-caps'],
   },
   mediumText: {
-    fontSize: 18,
-    fontWeight: '500',
+    fontSize: 20,
+    color: '#9c27b0',
     marginBottom: 10,
+    fontWeight: '500',
+    textAlign: 'right',
   },
   smallText: {
-    fontSize: 14,
+    fontSize: 15,
+    color: '#2196f3',
     fontWeight: '400',
+    textAlign: 'center',
   },
 });
+
