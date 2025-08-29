45a740caba272120eae5a0d6d4655ba56a0909f5
Zakhele <prince.feela2905@gmail.com>
2025-08-29 23:02:45 +0200

    .

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,7 +7,6 @@ export default function App() {
       {/* Main Entry Text */}
       <Text>Entry Screen - Awesome</Text>
 
-      {/* Additional Text Components */}
       <Text style={styles.largeText}>Typescript is great if you practice more</Text>
       <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
