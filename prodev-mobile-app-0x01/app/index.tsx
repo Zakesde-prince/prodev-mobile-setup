188a8a2e1ae653f63e399ea692bb08b97d057a2f
Zakhele <prince.feela2905@gmail.com>
2025-09-01 00:10:48 +0200

    .

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -14,12 +14,6 @@ export default function App() {
   );
 }
 
-<View>
-  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-  <Text style={styles.smallText}>ALX is awesome</Text>
-</View>
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
